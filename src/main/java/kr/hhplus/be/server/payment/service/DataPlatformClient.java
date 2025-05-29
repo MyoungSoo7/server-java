89a8784db5252efd5a176ae30df2e69432323438
LIM MYOUNGSOO <iamipro@naver.com>
2025-05-29 13:28:09 +0900

    kafka rbbitmq 세팅

--- src/main/java/kr/hhplus/be/server/payment/service/DataPlatformClient.java
+++ src/main/java/kr/hhplus/be/server/payment/service/DataPlatformClient.java
@@ -1,16 +1,28 @@
 package kr.hhplus.be.server.payment.service;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 import kr.hhplus.be.server.order.entity.Orders;
 
 @Component
 public class DataPlatformClient {
+	private static final String TOPIC = "order_topic";
+	private static final String EXCHANGE = "order.exchange";
+	private static final String ROUTING_KEY = "order.routingkey";
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
 
 	public void sendOrderToPlatform(Orders order) {
-		// 이곳에 외부 데이터 플랫폼 전송 로직 추가
-		// 예: Kafka 또는 RabbitMQ를 통해 데이터 전달
-		System.out.println("데이터 플랫폼에 주문 정보 전송: " + order.toString());
+		// Kafka에 메시지 전송
+		kafkaTemplate.send(TOPIC, order.toString());
+		// RabbitMQ에 메시지 전송
+		rabbitTemplate.convertAndSend(EXCHANGE, ROUTING_KEY, order.toString());
 	}
 }
 
