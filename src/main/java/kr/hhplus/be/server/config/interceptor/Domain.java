c7e022e19b7770be875c48bcfa7d71a225329fab
iamipro <iamipro@naver.com>
2025-07-18 22:16:55 +0900

    [pr]

--- src/main/java/kr/hhplus/be/server/config/interceptor/Domain.java
+++ src/main/java/kr/hhplus/be/server/config/interceptor/Domain.java
@@ -1,5 +1,10 @@
 package kr.hhplus.be.server.config.interceptor;
 
-//외부 메시지 전송 기능은 Kafka 대신 MockMessageProducer 또는 Outbox 테이블 방식으로 구
+/*
+ * 1. MockMessageProducer: 테스트 환경에서 메시지 전송을 모킹하여 실제 Kafka 의존성을 제거
+ * 2. Outbox 테이블: 데이터베이스에 메시지를 저장하고, 별도의 프로세스가 이를 읽어 Kafka로 전송
+ *
+ * 이 두 가지 방법은 Kafka 의존성을 줄이고, 테스트 용이성을 높이며, 시스템의 복잡성을 감소시킵니다.
+ */
 public class Domain {
 }
